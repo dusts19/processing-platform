9c8f1694e521ac854175398f6f48ecb7b980cf04
Dustin <dustin.j.shin@gmail.com>
2026-03-29 05:41:12 -0700

    Initial Spring Boot project setup, h2-console setup

---
+++ processing-platform-backend/src/main/java/com/dustin/processingplatformbackend/ProcessingPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dustin.processingplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ processing-platform-backend/src/test/java/com/dustin/processingplatformbackend/ProcessingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustin.processingplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
