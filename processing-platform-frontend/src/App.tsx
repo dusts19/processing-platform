97c42671b59e1e3b7f7756f99fdcf51c89b8f708
Dustin <dustin.j.shin@gmail.com>
2026-04-21 18:42:34 -0700

    feat(frontend:router): setup base routes and layout structure

---
+++ processing-platform-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/api-keys" element={<ApiKeysPage />}></Route>
+      <Route path="/playground" element={<PlaygroundPage />}></Route>
+      <Route path="/dashboard" element={<DashboardPage />}></Route>
+    </Routes>
+  )
+}
+
+export default App
