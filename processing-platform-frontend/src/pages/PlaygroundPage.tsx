844e3c65072555a042db12bc8214ac0368d919a9
Dustin <dustin.j.shin@gmail.com>
2026-04-23 18:25:42 -0700

    feat(frontend:playground): create API testing UI

---
+++ processing-platform-frontend/src/pages/PlaygroundPage.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+const PlaygroundPage = () => {
+    const [input, setInput] = useState("");
+    const [response, setResponse] = useState("");
+
+    
+    const handleSubmit = async (e : React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // TODO: create api/playgroundAPI.tsx with processInput()
+        setResponse(`You entered: ${input}`)
+
+    }
+
+    return(
+        <div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        name="process"
+                        type="text"
+                        value={input}
+                        placeholder="Enter text..."
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <div>
+                {response && <p>{response}</p>}
+            </div>
+        </div>
+    );
+}
+
+export default PlaygroundPage;
